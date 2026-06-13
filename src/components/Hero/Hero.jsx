fb334d40b297beb63da4650b69f3755fe412f9d6
Pragati Tayade <pragatitayade1302@gmail.com>
2026-06-13 17:16:51 +0530

    fix: replace expired Canva video URLs with working assets and links

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -87,7 +87,7 @@ export default function Hero() {
 
 
   const heroVideos = [
-    "https://template.canva.com/EAGsXwSDdUk/2/document_1440w-Fjnbdu4HcPk.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUH7DHWAQDT%2F20260608%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260608T203948Z&X-Amz-Expires=75672&X-Amz-Signature=1bb3b3a331874f056f9e4630b9435b590ae83bbe31b60f934b4bd38fa60c6e5a&X-Amz-SignedHeaders=host%3Bx-amz-expected-bucket-owner&response-expires=Tue%2C%2009%20Jun%202026%2017%3A41%3A00%20GMT",
+    "https://assets.mixkit.co/videos/preview/mixkit-close-up-of-a-woman-putting-on-a-gold-ring-41619-large.mp4",
     localHeroVideo2
   ];
 
